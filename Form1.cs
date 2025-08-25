3cf17430e4fedd5e2149663630816056cf464d57
vanesaarandavera <vanesaa715@gmail.com>
2025-08-24 21:02:35 -0300

    carga de tabla controlada

--- Form1.cs
+++ Form1.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Practico5
